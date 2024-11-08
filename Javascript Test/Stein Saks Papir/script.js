b8065a1bdb47e1d8675036336f663467cafcae95
williamnym13 <e-wilnym@iskule.no>
2024-11-08 11:39:02 +0100

    Saks of papir lagt til Stein Saks Papir spillet, har også fått up Datamaskinen til å funke.

--- Javascript Test/Stein Saks Papir/script.js
+++ Javascript Test/Stein Saks Papir/script.js
@@ -1,2 +1,58 @@
+// Variablene som definerer hva de tre elementene er
 let img_Stone = document.getElementById('Stone')
 let img_Paper = document.getElementById('Paper')
+let img_Scissors = document.getElementById('Scissors')
+
+let userChose = "";
+
+img_Stone.addEventListener('click', chosenStone);
+img_Paper.addEventListener('click', chosenPaper);
+img_Scissors.addEventListener('click', chosenScissors);
+
+
+function chosenStone () {
+    console.log('Du valgte stein!');
+    userChose = 'Stone';
+    gameResults();
+}
+
+function chosenPaper () {
+    console.log('Du valgte papir!');
+    userChose = 'Paper';
+    gameResults();
+}
+
+function chosenScissors () {
+    console.log('Du valgte saks!');
+    userChose = 'Scissors';
+    gameResults();
+}
+
+function gameResults () {
+    let computerChoose = Math.floor(Math.random()*3);
+    let chooseArray = ['Stone', 'Paper', 'Scissors']; 
+    computerChoose = chooseArray[computerChoose];
+    console.log('Datamaskinen valgte ' + computerChoose);
+
+    let endResult = '';
+
+    if (userChose == computerChoose) {
+        console.log('Uavgjort');
+    }
+
+    else if (userChose == 'Stone' && computerChoose == 'Scissors') {
+        console.log('Du vant!');
+    }
+
+    else if (userChose == 'Paper' && computerChoose == 'Stone') {
+        console.log('Du vant!');
+    }
+
+    else if (userChose == 'Scissors' && computerChoose == 'Paper') {
+        console.log('Du vant!');
+    }
+
+    else {
+        console.log('Datamaskinen vant...uff!')
+    }
+}
