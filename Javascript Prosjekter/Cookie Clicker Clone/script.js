358fe7c541fde14c7986c101433753c19f7ee7a0
williamnym13 <e-wilnym@iskule.no>
2024-12-05 15:44:09 +0100

    Videre jobbing på Cookie Clicker - Fikk det til å fungere

--- Javascript Prosjekter/Cookie Clicker Clone/script.js
+++ Javascript Prosjekter/Cookie Clicker Clone/script.js
@@ -1,11 +1,17 @@
 let currentCookies = 0;
 let totalCookies = 0;
-let cookieImage = document.getElementById('cookieImage');
+let cookieImage = document.getElementById('theBigCookie');
+let output = document.getElementById('cookieCount');
 
+cookieImage.addEventListener('click', cookieClick);
 
 function cookieClick() {
     currentCookies++;
-    console.log(currentCookies)
+    console.log(currentCookies);
+    output.innerText = currentCookies
 }
 
-cookieImage.addEventListener('click', cookieClick)
+function upgradeList() {
+    
+}
+
