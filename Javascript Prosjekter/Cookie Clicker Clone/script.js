08db1dacd86430ca991bb5774caef12eef1d41c3
williamnym13 <e-wilnym@iskule.no>
2024-12-09 10:37:53 +0100

    Fått multiplier til å fungere skikkelig, neste steg er å få en "autoclicker" fungerende

--- Javascript Prosjekter/Cookie Clicker Clone/script.js
+++ Javascript Prosjekter/Cookie Clicker Clone/script.js
@@ -1,17 +1,41 @@
+// Første elementer av spillet
 let currentCookies = 0;
-let totalCookies = 0;
+let upgradeCounter = 0;
+let multiplier = 1;
+let autoValue = 0;
+
+// Andre Elementer av spillet
 let cookieImage = document.getElementById('theBigCookie');
 let output = document.getElementById('cookieCount');
+let upgradeList = document.getElementById('Upgrader');
+
 
 cookieImage.addEventListener('click', cookieClick);
 
 function cookieClick() {
-    currentCookies++;
+    currentCookies = currentCookies + 1 * multiplier
     console.log(currentCookies);
-    output.innerText = currentCookies
+    output.innerText = currentCookies;
 }
 
-function upgradeList() {
-    
+
+
+document.getElementById('UpgradeOne').addEventListener('click', firstUpgrade);
+
+function firstUpgrade () {
+    if (currentCookies >= 10) {
+        currentCookies -= currentCookies;
+        multiplier += 1;
+    }
+    output.innerText = currentCookies;
 }
 
+function autoClicker () {
+    if (currentCookies >= 15) {
+        currentCookies -= currentCookies;
+        autoValue = 1;
+    }
+    if (autoValue = 1) {
+        setInterval (cookieClick, 1000);
+    }
+}
